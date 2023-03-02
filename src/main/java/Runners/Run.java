dccae2f1496f9bac87c2234124b7e22bea04b822
Suliman Behroz <sbehroz@nelta.de>
2023-03-02 15:19:16 +0100

    Initial version

---
+++ src/main/java/Runners/Run.java
@@ -0,0 +1,13 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "C:\\Users\\Suliman Behroz\\IdeaProjects\\GherkinTestDemo\\src\\main\\resources\\features\\LogInTest.feature",
+        glue = {"StepDefinitions"}
+)
+
+public class Run extends AbstractTestNGCucumberTests {
+}
