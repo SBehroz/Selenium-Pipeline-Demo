ab4dc0c1828e0433945032702fb9451fd3432a20
Suliman Behroz <sbehroz@nelta.de>
2023-03-03 10:40:43 +0100

    Initial version

--- src/main/java/Runners/Run.java
+++ src/main/java/Runners/Run.java
@@ -1,11 +1,10 @@
 package Runners;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-        features = "C:\\Users\\Suliman Behroz\\IdeaProjects\\GherkinTestDemo\\src\\main\\resources\\features\\LogInTest.feature",
+        features = "C:\\Users\\Suliman Behroz\\IdeaProjects\\GherkinTestDemo\\src\\main\\resources\\features",
         glue = {"StepDefinitions"}
 )
 
